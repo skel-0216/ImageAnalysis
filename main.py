d33d4dcf6da2ab3dae82b3e39d503236a39261ec
MinSeok <81317181+skel-0216@users.noreply.github.com>
2022-04-08 15:57:21 +0900

    data loaded
    image data loaded

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from mylib import *
 
 
 
-
+'''
 save_rgb_hist(path_maker('squid#1', 'stage00', 'body', 'bright', '00'), 'result/#1_00_body_bright_00')
 save_rgb_hist(path_maker('squid#1', 'stage00', 'body', 'bright', '01'), 'result/#1_00_body_bright_01')
 save_rgb_hist(path_maker('squid#1', 'stage00', 'body', 'bright', '02'), 'result/#1_00_body_bright_02')
@@ -26,7 +26,7 @@ save_rgb_hist(path_maker('squid#1', 'stage02', 'body', 'bright', '01'), 'result/
 save_rgb_hist(path_maker('squid#1', 'stage02', 'body', 'bright', '02'), 'result/#1_02_body_bright_02')
 save_rgb_hist(path_maker('squid#1', 'stage02', 'body', 'bright', '03'), 'result/#1_02_body_bright_03')
 save_rgb_hist(path_maker('squid#1', 'stage02', 'body', 'bright', '04'), 'result/#1_02_body_bright_04')
-
+'''
 '''
 save_rgb_hist_accumulate(path_maker('squid#1', 'stage00', 'body', 'bright', '00'), 'result/acc#1_00_body_bright_00')
 save_rgb_hist_accumulate(path_maker('squid#1', 'stage00', 'body', 'bright', '01'), 'result/acc#1_00_body_bright_01')
@@ -48,4 +48,25 @@ save_rgb_hist_accumulate(path_maker('squid#1', 'stage02', 'body', 'bright', '01'
 save_rgb_hist_accumulate(path_maker('squid#1', 'stage02', 'body', 'bright', '02'), 'result/acc#1_02_body_bright_02')
 save_rgb_hist_accumulate(path_maker('squid#1', 'stage02', 'body', 'bright', '03'), 'result/acc#1_02_body_bright_03')
 save_rgb_hist_accumulate(path_maker('squid#1', 'stage02', 'body', 'bright', '04'), 'result/acc#1_02_body_bright_04')
+'''
+'''
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage00', 'head', 'bright', '00'), 'result/acc#1_00_body_bright_00')
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage00', 'head', 'bright', '01'), 'result/acc#1_00_body_bright_01')
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage00', 'head', 'bright', '02'), 'result/acc#1_00_body_bright_02')
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage00', 'head', 'bright', '03'), 'result/acc#1_00_body_bright_03')
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage00', 'head', 'bright', '04'), 'result/acc#1_00_body_bright_04')
+'''
+'''
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage01', 'head', 'bright', '00'), 'result/acc#1_01_body_bright_00')
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage01', 'head', 'bright', '01'), 'result/acc#1_01_body_bright_01')
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage01', 'head', 'bright', '02'), 'result/acc#1_01_body_bright_02')
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage01', 'head', 'bright', '03'), 'result/acc#1_01_body_bright_03')
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage01', 'head', 'bright', '04'), 'result/acc#1_01_body_bright_04')
+'''
+'''
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage02', 'head', 'bright', '00'), 'result/acc#1_02_body_bright_00')
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage02', 'head', 'bright', '01'), 'result/acc#1_02_body_bright_01')
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage02', 'head', 'bright', '02'), 'result/acc#1_02_body_bright_02')
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage02', 'head', 'bright', '03'), 'result/acc#1_02_body_bright_03')
+save_rgb_hist_accumulate(path_maker('squid#1', 'stage02', 'head', 'bright', '04'), 'result/acc#1_02_body_bright_04')
 '''
