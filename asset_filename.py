687b35667a0ada8cb83ced8cd117c3cb388214cf
skeleton <skeleton2793@gmail.com>
2022-07-07 13:57:37 +0900

    asset_filename init

---
+++ asset_filename.py
@@ -0,0 +1,183 @@
+# file names
+# 한개에  stage별로 list를 나눔
+# ex) 해당 케이스의 case[0][1] 이면 스테이지0의 2번째 물고기
+
+# =======================================================      mackerel 2      =======================================================
+
+mackerel2_250_back_bright = [["source/mackerel#2/stage00/250/back/bright/01.png",
+                             "source/mackerel#2/stage00/250/back/bright/02.png",
+                             "source/mackerel#2/stage00/250/back/bright/03.png",
+                             "source/mackerel#2/stage00/250/back/bright/04.png",
+                             "source/mackerel#2/stage00/250/back/bright/05.png"],
+                             ["source/mackerel#2/stage01/250/back/bright/01.png",
+                              "source/mackerel#2/stage01/250/back/bright/02.png",
+                              "source/mackerel#2/stage01/250/back/bright/03.png",
+                              "source/mackerel#2/stage01/250/back/bright/04.png",
+                              "source/mackerel#2/stage01/250/back/bright/05.png"]]
+
+mackerel2_250_back_dark = [["source/mackerel#2/stage00/250/back/dark/01.png",
+                             "source/mackerel#2/stage00/250/back/dark/02.png",
+                             "source/mackerel#2/stage00/250/back/dark/03.png",
+                             "source/mackerel#2/stage00/250/back/dark/04.png",
+                             "source/mackerel#2/stage00/250/back/dark/05.png"],
+                             ["source/mackerel#2/stage01/250/back/dark/01.png",
+                              "source/mackerel#2/stage01/250/back/dark/02.png",
+                              "source/mackerel#2/stage01/250/back/dark/03.png",
+                              "source/mackerel#2/stage01/250/back/dark/04.png",
+                              "source/mackerel#2/stage01/250/back/dark/05.png"]]
+
+mackerel2_350_back_bright = [["source/mackerel#2/stage00/350/back/bright/01.png",
+                             "source/mackerel#2/stage00/350/back/bright/02.png",
+                             "source/mackerel#2/stage00/350/back/bright/03.png",
+                             "source/mackerel#2/stage00/350/back/bright/04.png",
+                             "source/mackerel#2/stage00/350/back/bright/05.png"],
+                             ["source/mackerel#2/stage01/350/back/bright/01.png",
+                              "source/mackerel#2/stage01/350/back/bright/02.png",
+                              "source/mackerel#2/stage01/350/back/bright/03.png",
+                              "source/mackerel#2/stage01/350/back/bright/04.png",
+                              "source/mackerel#2/stage01/350/back/bright/05.png"]]
+
+mackerel2_350_back_dark = [["source/mackerel#2/stage00/350/back/dark/01.png",
+                             "source/mackerel#2/stage00/350/back/dark/02.png",
+                             "source/mackerel#2/stage00/350/back/dark/03.png",
+                             "source/mackerel#2/stage00/350/back/dark/04.png",
+                             "source/mackerel#2/stage00/350/back/dark/05.png"],
+                             ["source/mackerel#2/stage01/350/back/dark/01.png",
+                              "source/mackerel#2/stage01/350/back/dark/02.png",
+                              "source/mackerel#2/stage01/350/back/dark/03.png",
+                              "source/mackerel#2/stage01/350/back/dark/04.png",
+                              "source/mackerel#2/stage01/350/back/dark/05.png"]]
+
+mackerel2_250_stomach_bright = [["source/mackerel#2/stage00/250/stomach/bright/01.png",
+                             "source/mackerel#2/stage00/250/stomach/bright/02.png",
+                             "source/mackerel#2/stage00/250/stomach/bright/03.png",
+                             "source/mackerel#2/stage00/250/stomach/bright/04.png",
+                             "source/mackerel#2/stage00/250/stomach/bright/05.png"],
+                             ["source/mackerel#2/stage01/250/stomach/bright/01.png",
+                              "source/mackerel#2/stage01/250/stomach/bright/02.png",
+                              "source/mackerel#2/stage01/250/stomach/bright/03.png",
+                              "source/mackerel#2/stage01/250/stomach/bright/04.png",
+                              "source/mackerel#2/stage01/250/stomach/bright/05.png"]]
+
+mackerel2_250_stomach_dark = [["source/mackerel#2/stage00/250/stomach/dark/01.png",
+                             "source/mackerel#2/stage00/250/stomach/dark/02.png",
+                             "source/mackerel#2/stage00/250/stomach/dark/03.png",
+                             "source/mackerel#2/stage00/250/stomach/dark/04.png",
+                             "source/mackerel#2/stage00/250/stomach/dark/05.png"],
+                             ["source/mackerel#2/stage01/250/stomach/dark/01.png",
+                              "source/mackerel#2/stage01/250/stomach/dark/02.png",
+                              "source/mackerel#2/stage01/250/stomach/dark/03.png",
+                              "source/mackerel#2/stage01/250/stomach/dark/04.png",
+                              "source/mackerel#2/stage01/250/stomach/dark/05.png"]]
+
+mackerel2_350_stomach_bright = [["source/mackerel#2/stage00/350/stomach/bright/01.png",
+                             "source/mackerel#2/stage00/350/stomach/bright/02.png",
+                             "source/mackerel#2/stage00/350/stomach/bright/03.png",
+                             "source/mackerel#2/stage00/350/stomach/bright/04.png",
+                             "source/mackerel#2/stage00/350/stomach/bright/05.png"],
+                             ["source/mackerel#2/stage01/350/stomach/bright/01.png",
+                              "source/mackerel#2/stage01/350/stomach/bright/02.png",
+                              "source/mackerel#2/stage01/350/stomach/bright/03.png",
+                              "source/mackerel#2/stage01/350/stomach/bright/04.png",
+                              "source/mackerel#2/stage01/350/stomach/bright/05.png"]]
+
+mackerel2_350_stomach_dark = [["source/mackerel#2/stage00/350/stomach/dark/01.png",
+                             "source/mackerel#2/stage00/350/stomach/dark/02.png",
+                             "source/mackerel#2/stage00/350/stomach/dark/03.png",
+                             "source/mackerel#2/stage00/350/stomach/dark/04.png",
+                             "source/mackerel#2/stage00/350/stomach/dark/05.png"],
+                             ["source/mackerel#2/stage01/350/stomach/dark/01.png",
+                              "source/mackerel#2/stage01/350/stomach/dark/02.png",
+                              "source/mackerel#2/stage01/350/stomach/dark/03.png",
+                              "source/mackerel#2/stage01/350/stomach/dark/04.png",
+                              "source/mackerel#2/stage01/350/stomach/dark/05.png"]]
+
+# =======================================================      mackerel 3      =======================================================
+
+mackerel3_250_back_bright = [["source/mackerel#3/stage00/250/back/bright/01.png",
+                             "source/mackerel#3/stage00/250/back/bright/02.png",
+                             "source/mackerel#3/stage00/250/back/bright/03.png",
+                             "source/mackerel#3/stage00/250/back/bright/04.png",
+                             "source/mackerel#3/stage00/250/back/bright/05.png"],
+                             ["source/mackerel#3/stage01/250/back/bright/01.png",
+                              "source/mackerel#3/stage01/250/back/bright/02.png",
+                              "source/mackerel#3/stage01/250/back/bright/03.png",
+                              "source/mackerel#3/stage01/250/back/bright/04.png",
+                              "source/mackerel#3/stage01/250/back/bright/05.png"]]
+
+mackerel3_250_back_dark = [["source/mackerel#3/stage00/250/back/dark/01.png",
+                             "source/mackerel#3/stage00/250/back/dark/02.png",
+                             "source/mackerel#3/stage00/250/back/dark/03.png",
+                             "source/mackerel#3/stage00/250/back/dark/04.png",
+                             "source/mackerel#3/stage00/250/back/dark/05.png"],
+                             ["source/mackerel#3/stage01/250/back/dark/01.png",
+                              "source/mackerel#3/stage01/250/back/dark/02.png",
+                              "source/mackerel#3/stage01/250/back/dark/03.png",
+                              "source/mackerel#3/stage01/250/back/dark/04.png",
+                              "source/mackerel#3/stage01/250/back/dark/05.png"]]
+
+mackerel3_350_back_bright = [["source/mackerel#3/stage00/350/back/bright/01.png",
+                             "source/mackerel#3/stage00/350/back/bright/02.png",
+                             "source/mackerel#3/stage00/350/back/bright/03.png",
+                             "source/mackerel#3/stage00/350/back/bright/04.png",
+                             "source/mackerel#3/stage00/350/back/bright/05.png"],
+                             ["source/mackerel#3/stage01/350/back/bright/01.png",
+                              "source/mackerel#3/stage01/350/back/bright/02.png",
+                              "source/mackerel#3/stage01/350/back/bright/03.png",
+                              "source/mackerel#3/stage01/350/back/bright/04.png",
+                              "source/mackerel#3/stage01/350/back/bright/05.png"]]
+
+mackerel3_350_back_dark = [["source/mackerel#3/stage00/350/back/dark/01.png",
+                             "source/mackerel#3/stage00/350/back/dark/02.png",
+                             "source/mackerel#3/stage00/350/back/dark/03.png",
+                             "source/mackerel#3/stage00/350/back/dark/04.png",
+                             "source/mackerel#3/stage00/350/back/dark/05.png"],
+                             ["source/mackerel#3/stage01/350/back/dark/01.png",
+                              "source/mackerel#3/stage01/350/back/dark/02.png",
+                              "source/mackerel#3/stage01/350/back/dark/03.png",
+                              "source/mackerel#3/stage01/350/back/dark/04.png",
+                              "source/mackerel#3/stage01/350/back/dark/05.png"]]
+
+mackerel3_250_stomach_bright = [["source/mackerel#3/stage00/250/stomach/bright/01.png",
+                             "source/mackerel#3/stage00/250/stomach/bright/02.png",
+                             "source/mackerel#3/stage00/250/stomach/bright/03.png",
+                             "source/mackerel#3/stage00/250/stomach/bright/04.png",
+                             "source/mackerel#3/stage00/250/stomach/bright/05.png"],
+                             ["source/mackerel#3/stage01/250/stomach/bright/01.png",
+                              "source/mackerel#3/stage01/250/stomach/bright/02.png",
+                              "source/mackerel#3/stage01/250/stomach/bright/03.png",
+                              "source/mackerel#3/stage01/250/stomach/bright/04.png",
+                              "source/mackerel#3/stage01/250/stomach/bright/05.png"]]
+
+mackerel3_250_stomach_dark = [["source/mackerel#3/stage00/250/stomach/dark/01.png",
+                             "source/mackerel#3/stage00/250/stomach/dark/02.png",
+                             "source/mackerel#3/stage00/250/stomach/dark/03.png",
+                             "source/mackerel#3/stage00/250/stomach/dark/04.png",
+                             "source/mackerel#3/stage00/250/stomach/dark/05.png"],
+                             ["source/mackerel#3/stage01/250/stomach/dark/01.png",
+                              "source/mackerel#3/stage01/250/stomach/dark/02.png",
+                              "source/mackerel#3/stage01/250/stomach/dark/03.png",
+                              "source/mackerel#3/stage01/250/stomach/dark/04.png",
+                              "source/mackerel#3/stage01/250/stomach/dark/05.png"]]
+
+mackerel3_350_stomach_bright = [["source/mackerel#3/stage00/350/stomach/bright/01.png",
+                             "source/mackerel#3/stage00/350/stomach/bright/02.png",
+                             "source/mackerel#3/stage00/350/stomach/bright/03.png",
+                             "source/mackerel#3/stage00/350/stomach/bright/04.png",
+                             "source/mackerel#3/stage00/350/stomach/bright/05.png"],
+                             ["source/mackerel#3/stage01/350/stomach/bright/01.png",
+                              "source/mackerel#3/stage01/350/stomach/bright/02.png",
+                              "source/mackerel#3/stage01/350/stomach/bright/03.png",
+                              "source/mackerel#3/stage01/350/stomach/bright/04.png",
+                              "source/mackerel#3/stage01/350/stomach/bright/05.png"]]
+
+mackerel3_350_stomach_dark = [["source/mackerel#3/stage00/350/stomach/dark/01.png",
+                             "source/mackerel#3/stage00/350/stomach/dark/02.png",
+                             "source/mackerel#3/stage00/350/stomach/dark/03.png",
+                             "source/mackerel#3/stage00/350/stomach/dark/04.png",
+                             "source/mackerel#3/stage00/350/stomach/dark/05.png"],
+                             ["source/mackerel#3/stage01/350/stomach/dark/01.png",
+                              "source/mackerel#3/stage01/350/stomach/dark/02.png",
+                              "source/mackerel#3/stage01/350/stomach/dark/03.png",
+                              "source/mackerel#3/stage01/350/stomach/dark/04.png",
+                              "source/mackerel#3/stage01/350/stomach/dark/05.png"]]
